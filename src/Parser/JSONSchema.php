0dc9f4ebc8641a4430f3318fc3f154412266f147
Muqsit <hackhack05@gmail.com>
2025-03-24 09:07:00 +0000

    schema: add a code param

--- src/Parser/JSONSchema.php
+++ src/Parser/JSONSchema.php
@@ -27,15 +27,16 @@ final class JSONSchema{
 	 * @param array $schema a schema detailing structure and type
 	 * @param list<string|int> $base base directory to move to in $json
 	 * @param int-mask-of<self::FLAG_*> $flags
+	 * @param int $code error code to use when an error encounters
 	 */
-	public static function validate(mixed $json, array $schema, array $base = [], int $flags = 0) : void{
+	public static function validate(mixed $json, array $schema, array $base = [], int $flags = 0, int $code = GLTFParser::ERR_INVALID_SCHEMA) : void{
 		$optional = ($flags & self::FLAG_OPTIONAL) > 0;
 		$report_unknown_k = ($flags & self::FLAG_REPORT_UNKNOWN_KEYS) > 0;
 		$nesting = ($flags & self::FLAG_NO_NESTING) === 0;
 
 		foreach($base as $offset => $key){
-			is_array($json) || throw new InvalidArgumentException("Directory /" . implode("/", array_slice($base, 0, $offset + 1)) . " must be an array", self::ERR_INVALID_SCHEMA);
-			isset($json[$key]) || throw new InvalidArgumentException("Directory /" . implode("/", array_slice($base, 0, $offset + 1)) . " must be set", self::ERR_INVALID_SCHEMA);
+			is_array($json) || throw new InvalidArgumentException("Directory /" . implode("/", array_slice($base, 0, $offset + 1)) . " must be an array", $code);
+			isset($json[$key]) || throw new InvalidArgumentException("Directory /" . implode("/", array_slice($base, 0, $offset + 1)) . " must be set", $code);
 			$json = $json[$key];
 		}
 
@@ -51,19 +52,19 @@ final class JSONSchema{
 			$directory_cur = $directory;
 			foreach($keys as $key){
 				$directory_cur[] = $key;
-				is_array($value) || throw new InvalidArgumentException("Directory /" . implode("/", $directory_cur) . " must be an array", self::ERR_INVALID_SCHEMA);
+				is_array($value) || throw new InvalidArgumentException("Directory /" . implode("/", $directory_cur) . " must be an array", $code);
 				if($report_unknown_k && is_array($sub_schema)){
 					$unknown_k = array_keys(array_diff_key($value, $sub_schema));
-					count($unknown_k) === 0 || throw new InvalidArgumentException("Unknown sub-directory encountered in /" . implode("/", $directory_cur) . ": [" . implode(", ", array_slice($unknown_k, 0, 8)) . "], expected one of: [" . implode(", ", array_keys($sub_schema)) . "]", self::ERR_INVALID_SCHEMA);
+					count($unknown_k) === 0 || throw new InvalidArgumentException("Unknown sub-directory encountered in /" . implode("/", $directory_cur) . ": [" . implode(", ", array_slice($unknown_k, 0, 8)) . "], expected one of: [" . implode(", ", array_keys($sub_schema)) . "]", $code);
 				}
 				if($optional && !isset($value[$key])){
 					continue 2;
 				}
-				isset($value[$key]) || throw new InvalidArgumentException("Directory /" . implode("/", $directory_cur) . " must be set", self::ERR_INVALID_SCHEMA);
+				isset($value[$key]) || throw new InvalidArgumentException("Directory /" . implode("/", $directory_cur) . " must be set", $code);
 				$value = $value[$key];
 				$sub_schema = $sub_schema[$key];
 			}
-			gettype($value) === gettype($expect) || (is_float($expect) && is_int($value)) || throw new InvalidArgumentException("Expected type of value at /" . implode("/", $directory_cur) . " to be " . gettype($expect) . ", got " . gettype($value), self::ERR_INVALID_SCHEMA);
+			gettype($value) === gettype($expect) || (is_float($expect) && is_int($value)) || throw new InvalidArgumentException("Expected type of value at /" . implode("/", $directory_cur) . " to be " . gettype($expect) . ", got " . gettype($value), $code);
 			if(is_array($expect) && $nesting){
 				foreach($expect as $k => $v){
 					$keys2 = $keys;
