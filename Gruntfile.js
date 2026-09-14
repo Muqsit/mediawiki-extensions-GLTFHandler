9be76a3e48a96323775ed8e8c8f403eb3f0061ef
Muqsit Rayyan <notmuqsit@gmail.com>
2026-09-14 23:45:58 +0800

    fix 4073855: failing npm tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,7 @@ module.exports = function ( grunt ) {
 			},
 			all: [
 				'**/*.{css,less}',
+				'!mediawiki/**',
 				'!node_modules/**',
 				'!vendor/**'
 			]
--- resources/ext.gltfHandler/model-viewer-dynsize.js
+++ resources/ext.gltfHandler/model-viewer-dynsize.js
@@ -1,20 +1,20 @@
-$(() => {
-    // simulate width=100%, height=auto using javascript
-    const elements = document.querySelectorAll(".model-viewer-dynsize[data-width][data-height]")
-    window.addEventListener("resize", _ => {
-        for(const element of elements){
-            const figure_node = element.closest("figure")
-            if(figure_node === null || figure_node.parentNode === null){
-                continue
-            }
+$( () => {
+	// simulate width=100%, height=auto using javascript
+	const elements = document.querySelectorAll( '.model-viewer-dynsize[data-width][data-height]' );
+	window.addEventListener( 'resize', () => {
+		for ( const element of elements ) {
+			const figureNode = element.closest( 'figure' );
+			if ( figureNode === null || figureNode.parentNode === null ) {
+				continue;
+			}
 
-            const width = parseFloat(element.getAttribute("data-width"))
-            const height = parseFloat(element.getAttribute("data-height"))
-            const available_space = figure_node.parentNode.getBoundingClientRect()
+			const width = parseFloat( element.getAttribute( 'data-width' ) );
+			const height = parseFloat( element.getAttribute( 'data-height' ) );
+			const availableSpace = figureNode.parentNode.getBoundingClientRect();
 
-            const new_width = Math.min(available_space["width"], width)
-            element.style.width = new_width + "px"
-            element.style.height = ((height / width) * new_width) + "px"
-        }
-    })
-})
+			const newWidth = Math.min( availableSpace.width, width );
+			element.style.width = newWidth + 'px';
+			element.style.height = ( ( height / width ) * newWidth ) + 'px';
+		}
+	} );
+} );
